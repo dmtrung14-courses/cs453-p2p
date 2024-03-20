8d1a86cf0169db4b526aa9ccd54199429aaa11bb
Trung Dang <trungdangminh14012004@gmail.com>
2024-03-19 23:55:30 -0400

    project1a done fetching failed submissions

---
+++ project1/CSDownloader.py
@@ -0,0 +1,48 @@
+from socket import timeout, socket, AF_INET, SOCK_STREAM
+import io
+from PIL import Image
+import sys
+
+def download_image(filename, address, port):
+    # Create a socket object
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    # Define the server address and port
+    server_address = (address, port)
+    # Connect to the server
+    client_socket.connect(server_address)
+    # Send the request to download the file
+    request = f"GET {filename}\n"
+    client_socket.send(request.encode())
+    print("Connected to Server. Requesting file...")
+    data = b""
+    # Receive the response from the server
+    while True:
+        try:
+            response = client_socket.recv(1024)
+            data += response
+            client_socket.settimeout(5)
+        except timeout:
+            break
+
+    print("Done getting files, preparing to save...")
+    header_sep = data.index(b'\n\n')
+    file = data[header_sep+2:]
+
+    # Create an image object from the bytes
+    image = Image.open(io.BytesIO(file)).convert("RGB")
+
+    # Save the image file
+    image.save(filename)
+
+    client_socket.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python CSDownloader.py <filename> <address> <port>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    address = sys.argv[2]
+    port = int(sys.argv[3])
+
+    download_image(filename, address, port)
